819dc22969b8e58c407ab0a2e438044eac9e8d1c
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-11-09 12:26:29 -0800

    first commit

---
+++ Stacks/validParentheses.js
@@ -0,0 +1,18 @@
+
+var isValid = function(s) {
+    if(s.length === 0) return true;
+    
+    const stack = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        if(parens[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            const leftBracket = stack.pop();
+            const correctBracket = parens[leftBracket];
+            if(s[i] !== correctBracket) return false
+        }
+    }
+    
+    return stack.length === 0;
+};
